eaeeb72c0e7b05b66307277f78a37c803ebf10ce
Wilson422 <42242100@yahoo.com.tw>
2025-07-25 11:27:40 +0800

    首次上傳

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "photo-to-pdf-cache-v1";
+const urlsToCache = ["./", "./index.html", "./manifest.json", "./icon.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
