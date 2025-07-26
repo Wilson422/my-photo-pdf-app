faad5144188f7a80aa1feac43455e865f904909e
Wilson422 <42242100@yahoo.com.tw>
2025-07-26 16:02:20 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_VERSION = 'v20250726-1';
+const CACHE_VERSION = 'v20250726-6';
 const CACHE_NAME = `sw-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [
   '/', // 根目錄
-  '/index.html?v=20250726-1',
-  '/manifest.json?v=20250726-1',
-  '/icon.png?v=20250726-1',
-  '/service-worker.js?v=20250726-1',
+  '/index.html?v=20250726-6',
+  '/manifest.json?v=20250726-6',
+  '/icon.png?v=20250726-6',
+  '/service-worker.js?v=20250726-6',
   'https://unpkg.com/cropperjs@1.5.13/dist/cropper.min.css',
   'https://unpkg.com/cropperjs@1.5.13/dist/cropper.min.js',
   'https://cdn.jsdelivr.net/npm/jspdf@2.5.1/dist/jspdf.umd.min.js'
